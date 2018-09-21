f35ed292f583ae16732c85110a2cfa2a6a25254b
Jose Guzman <jguzmanromero@deloitte.es>
2018-09-21 09:27:35 +0200

    Primer cambio

--- src/main/java/com/trendingdev/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/trendingdev/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,6 +3,8 @@ package com.trendingdev.microservices.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
+//Primer cambio
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
